051c10a7ba39924ed00a714c437c6eb3ea81d43a
Pedro Franceschi <117317386+FranceschiPedro@users.noreply.github.com>
2026-02-02 02:06:09 +0000

    Before Tutoring

--- src/app.py
+++ src/app.py
@@ -32,12 +32,28 @@ def sitemap():
 @app.route('/members', methods=['GET'])
 def handle_hello():
     # This is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    all_members = jackson_family.get_all_members()
+    return jsonify(all_members), 200
 
+@app.route('/members', method=['POST'])
+def add_member():
+    body = request.get_json()
+    new_member = jackson_family.add_member(body)
+    return jsonify(new_member), 200
 
+@app.route('/members/<int:id>', methods=['GET'])
+def get_single_member(id):
+    member = jackson_family.get_member(id)
+    if member is None:
+        return jsonify({"error": "Member not found"}), 404
+    return jsonify(member), 
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    deleted = jackson_family.delete_member(id)
+    if deleted is None:
+        return jsonify({"error": "Member not found"}), 404
+    return jsonify({"done": True}), 200
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
